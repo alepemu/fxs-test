a21ac736eaee763bee35b287f2ed383f1d81d661
Alejandro Penalver Munita <geo.al3x@gmail.com>
2024-06-18 08:42:23 +0200

    Update api.ts url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 import { PostProps } from "@/types";
 
-// const url = "https://run.mocky.io/v3/96314262-67b1-455b-a2b2-ef1711d4634c";
-const url = "https://run.mocky.io/v3/6c6371eb-e63d-4f31-944e-74013695680e";
+const url = "https://fxsfrontend.fxstreet.workers.dev/";
 
 export const getAPIwithDelay = async (delay: number): Promise<PostProps[]> => {
   return new Promise((resolve) => {
